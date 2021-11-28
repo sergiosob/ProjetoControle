4edeaf3a6bb0b08d0f63ba95e73556932dbfe646
sergiosob <sobrito@gmail.com>
2021-11-28 09:25:50 -0300

    exemplo de profile

---
+++ ProjetoControle/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { ProfileComponent } from './componentes/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
